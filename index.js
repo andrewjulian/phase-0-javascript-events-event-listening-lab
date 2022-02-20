50dbab65f2b13ebf19f99a72926c60bedb1d0631
+ <+>
2022-02-19 17:38:32 -0700

    commit tree c5e400579777444eb53bcdf0e03b0c1c8f428b90

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function addingEventListener() {
     const input = document.getElementById('input');
-    input.addEventListener('click', function() {
-    alert('I was clicked!');
-});
+    input.addEventListener('click', () => alert('I was clicked!'))
 }
